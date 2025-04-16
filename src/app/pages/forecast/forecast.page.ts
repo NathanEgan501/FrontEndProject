ad56332168b8038815995cfc043cecfcbeec35ac
NATHAN EGAN <G00435730@atu.ie>
2025-04-16 16:48:13 +0100

    Made changes to the css and also added strorage

--- src/app/pages/forecast/forecast.page.ts
+++ src/app/pages/forecast/forecast.page.ts
@@ -4,33 +4,52 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { WeatherService } from '../../services/weather.service';
-import { HttpClientModule } from '@angular/common/http';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.page.html',
   styleUrls: ['./forecast.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterModule, FormsModule, HttpClientModule]
+  imports: [IonicModule, CommonModule, RouterModule, FormsModule],  // Make sure FormsModule is imported here
 })
 export class ForecastPage implements OnInit {
   city: string = '';
   weatherData: any = null;
+  loading: boolean = false;
 
-  constructor(private weatherService: WeatherService, private toastController: ToastController) {}
+  constructor(
+    private weatherService: WeatherService,
+    private toastController: ToastController,
+    private storage: Storage
+  ) {}
 
   ngOnInit() {
-    this.getWeatherByLocation();
+    this.loadLastSearchedCity();
+  }
+
+  async loadLastSearchedCity() {
+    const savedCity = await this.storage.get('preferredCity');
+    if (savedCity) {
+      this.city = savedCity;
+      this.getWeather();
+    }
   }
 
   async getWeatherByLocation() {
     try {
+      this.loading = true;
       const position = await this.getCurrentPosition();
-      this.weatherService.getWeatherByLocation(position.coords.latitude, position.coords.longitude) // <-- Fixed this line
+      this.weatherService.getWeatherByLocation(position.coords.latitude, position.coords.longitude)
         .subscribe(data => {
           this.weatherData = data;
+          this.loading = false;
+        }, error => {
+          this.loading = false;
+          this.presentToast('Failed to fetch weather by location');
         });
     } catch (error) {
+      this.loading = false;
       this.presentToast('Location access denied or unavailable.');
     }
   }
@@ -41,11 +60,15 @@ export class ForecastPage implements OnInit {
       return;
     }
 
+    this.loading = true;
     this.weatherService.getWeatherByCity(this.city).subscribe(
       data => {
         this.weatherData = data;
+        this.loading = false;
+        this.storage.set('preferredCity', this.city);  // Save the city to storage
       },
       error => {
+        this.loading = false;
         this.presentToast('City not found.');
       }
     );
@@ -59,7 +82,7 @@ export class ForecastPage implements OnInit {
       );
     });
   }
-  
+
   private async presentToast(message: string) {
     const toast = await this.toastController.create({
       message,
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { PLATFORM_ID, inject } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';  // <-- New approach
+import { provideStorage, Storage } from '@ionic/storage-angular';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
@@ -10,6 +12,13 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideHttpClient(withInterceptorsFromDi()),  
+    provideHttpClient(),
+    {
+      provide: Storage,
+      useFactory: () => {
+        const platformId = inject(PLATFORM_ID);
+        return provideStorage(platformId, { name: '__mydb' });
+      }
+    }
   ],
 });
