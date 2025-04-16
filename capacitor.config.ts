abaeabec378cb5a64c921a796811ffd2a1994c8c
NATHAN EGAN <G00435730@atu.ie>
2025-04-16 12:22:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FrontEndProject',
+  webDir: 'www'
+};
+
+export default config;
